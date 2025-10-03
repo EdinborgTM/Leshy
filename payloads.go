873a9ebdb9c02c72d8609ee1c5e0d5f1bdc6cbd5
EdinborgTM <javerno404@gmail.com>
2025-10-03 04:49:21 +0330

    Payloads

--- payloads.go
+++ payloads.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
-	"math/rand/v2"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 // GirPayloadUDP: Payload-haye pishrafte baraye UDP
@@ -12,19 +13,30 @@ func GirPayloadUDP(noepayload string) []byte {
 	switch strings.ToLower(noepayload) {
 	case "dns":
 		// DNS Query pishrafte ba ID tasadofi baraye A record example.com
-		id := rand.Uint16()
+		rand.Seed(time.Now().UnixNano())
+		id := uint16(rand.Intn(65536))
 		header := fmt.Sprintf("%04x01000001000000000000076578616d706c6503636f6d0000010001", id)
 		data, _ := hex.DecodeString(header)
 		return data
+	case "version.bind":
+		// DNS Query baraye version.bind (TXT record, CHAOS class)
+		rand.Seed(time.Now().UnixNano())
+		id := uint16(rand.Intn(65536))
+		header := fmt.Sprintf("%04x010000010000000000000776657273696f6e0462696e640000100003", id)
+		data, _ := hex.DecodeString(header)
+		return data
 	case "ntp":
-		// NTP Monlist Request pishrafte baraye estekhraj noskhe va info
-		data, _ := hex.DecodeString("17 00 03 2a 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
+		// NTP Request sadeh-tar baraye estekhraj noskhe
+		data, _ := hex.DecodeString("1b00000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
 		return data
 	case "snmp":
-		// SNMP GetBulkRequest pishrafte baraye sysDescr va sysObjectID
-		data, _ := hex.DecodeString("30 2e 02 01 00 04 06 70 75 62 6c 69 63 a5 21 02 04 00 00 00 01 02 01 00 02 01 00 30 13 30 11 06 0d 2b 06 01 02 01 01 03 00 05 00 30 0f 06 0b 2b 06 01 02 01 01 01 00 05 00")
+		// SNMP GetRequest sadeh-tar
+		data, _ := hex.DecodeString("302902010004067075626c6963a01e0204000000010201000201003010300e060a2b060102010101000500")
 		return data
+	case "test":
+		// Payload sadeh baraye test
+		return []byte("TEST")
 	default:
-		return []byte{0x00} // Minimal probe for no payload
+		return []byte{0x00} // Minimal probe
 	}
 }
